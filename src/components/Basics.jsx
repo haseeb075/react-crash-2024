bc50a62b8ee21e0bd8b3509b414ee5220f20ae28
Haseeb <haseeb@192.168.1.3>
2024-05-05 05:43:10 +0500

    New Change

--- src/components/Basics.jsx
+++ src/components/Basics.jsx
@@ -12,7 +12,7 @@ const Basics = () => {
   return (
     <>
     <div className='text-5xl'>
-      This is main app component
+      This is main app component in react
     </div>
     <p style={styles}>hello {name} Age {10}</p>
     <ul>
