e84b6a4384e01699f2330a2a6097bbbfdea322b5
haseeb075 <haseebiqbal000@gmail.com>
2024-09-30 17:22:57 +0500

    New things

--- src/components/JobsList.jsx
+++ src/components/JobsList.jsx
@@ -44,7 +44,7 @@ const JobsList = () => {
                     href="job.html"
                     className="h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm"
                   >
-                    Read More
+                    Read More one moree time 
                   </a>
                 </div>
               </div>
